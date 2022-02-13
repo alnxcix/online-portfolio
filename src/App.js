be66697f4a833be88e9fa121475a55c22c1da3c0
Roy Allen Nidoy <royallen.nidoy.iics@ust.edu.ph>
2022-02-13 18:11:36 +0800

    implemented lazy loading for custom components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // import packages
-import { useState } from "react";
-
-// import custom components
-import About from "./components/About";
-import Education from "./components/Education";
-import Experiences from "./components/Experiences";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import { lazy, Suspense, useState } from "react";
 
 // import assets
 import Avatar from "./assets/images/avatar.jfif";
+
+// lazy load components
+const About = lazy(() => import("./components/About"));
+const Education = lazy(() => import("./components/Education"));
+const Experiences = lazy(() => import("./components/Experiences"));
+const Projects = lazy(() => import("./components/Projects"));
+const Skills = lazy(() => import("./components/Skills"));
 
 const App = () => {
   const [activeBtn, setActiveBtn] = useState("About");
@@ -46,15 +46,25 @@ const App = () => {
           <div className="card w-100 rounded-3 shadow">
             <div className="card-body">
               {activeBtn === "About" ? (
-                <About />
+                <Suspense fallback={<div className="spinner-border" />}>
+                  <About />
+                </Suspense>
               ) : activeBtn === "Education" ? (
-                <Education />
+                <Suspense fallback={<div className="spinner-border" />}>
+                  <Education />
+                </Suspense>
               ) : activeBtn === "Experiences" ? (
-                <Experiences />
+                <Suspense fallback={<div className="spinner-border" />}>
+                  <Experiences />
+                </Suspense>
               ) : activeBtn === "Projects" ? (
-                <Projects />
+                <Suspense fallback={<div className="spinner-border" />}>
+                  <Projects />
+                </Suspense>
               ) : (
-                <Skills />
+                <Suspense fallback={<div className="spinner-border" />}>
+                  <Skills />
+                </Suspense>
               )}
             </div>
           </div>
