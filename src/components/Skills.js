cfd8e868e6dd516b42f4ad1a981ac5fb65cb4cf4
Roy Allen Nidoy <royallen.nidoy.iics@ust.edu.ph>
2022-02-19 10:19:51 +0800

    added svgs from simple icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,24 @@
+import amazonaws from "../assets/svg/amazonaws.svg";
+import androidstudio from "../assets/svg/androidstudio.svg";
+import bootstrap from "../assets/svg/bootstrap.svg";
+import css3 from "../assets/svg/css3.svg";
+import dotnet from "../assets/svg/dotnet.svg";
+import electron from "../assets/svg/electron.svg";
+import git from "../assets/svg/git.svg";
+import html5 from "../assets/svg/html5.svg";
+import java from "../assets/svg/java.svg";
+import javascript from "../assets/svg/javascript.svg";
+import mongodb from "../assets/svg/mongodb.svg";
+import mysql from "../assets/svg/mysql.svg";
+import nextdotjs from "../assets/svg/nextdotjs.svg";
+import nodedotjs from "../assets/svg/nodedotjs.svg";
+import oracle from "../assets/svg/oracle.svg";
+import php from "../assets/svg/php.svg";
+import phpmyadmin from "../assets/svg/phpmyadmin.svg";
+import python from "../assets/svg/python.svg";
+import react from "../assets/svg/react.svg";
+import tailwindcss from "../assets/svg/tailwindcss.svg";
+
 const Skills = () => (
   <>
     <h2 className="fw-bold">
@@ -7,11 +28,11 @@ const Skills = () => (
       <p className="text-muted">
         Languages, tools, and frameworks that I am most comfortable with.
       </p>
-      {["fa-html5", "fa-bootstrap", "fa-js", "fa-react", "fa-npm"].map(
-        (e, i) => (
-          <i className={`custom-icon fa-brands ${e} fa-3x me-4`} key={i} />
-        )
-      )}
+      {[html5, css3, javascript, bootstrap, react].map((e, i) => (
+        <span key={i}>
+          <img alt="icon" className="mb-3" src={e} height="50" />{" "}
+        </span>
+      ))}
       <hr />
     </div>
     <h2 className="fw-bold mb-3">
@@ -22,15 +43,22 @@ const Skills = () => (
         Languages and tools that I also work (or used to work) with.
       </p>
       {[
-        "fa-java",
-        "fa-css3",
-        "fa-github",
-        "fa-node",
-        "fa-android",
-        "fa-python",
-        "fa-php",
+        java,
+        oracle,
+        git,
+        nodedotjs,
+        mongodb,
+        androidstudio,
+        electron,
+        dotnet,
+        python,
+        php,
+        phpmyadmin,
+        mysql,
       ].map((e, i) => (
-        <i className={`custom-icon fa-brands ${e} fa-3x me-4`} key={i} />
+        <span key={i}>
+          <img alt="icon" className="mb-3" src={e} height="50" />{" "}
+        </span>
       ))}
       <hr />
     </div>
@@ -39,7 +67,11 @@ const Skills = () => (
     </h2>
     <div className="ms-5">
       <p className="text-muted">Tools on their way to the lists above.</p>
-      <i className="custom-icon fa-brands fa-aws fa-3x me-4" />
+      {[amazonaws, tailwindcss, nextdotjs].map((e, i) => (
+        <span key={i}>
+          <img alt="icon" className="mb-3" src={e} height="50" />{" "}
+        </span>
+      ))}
     </div>
   </>
 );
