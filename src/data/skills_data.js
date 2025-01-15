b38e493f6255ed940dfb1a3f4b2b53883cb367c1
Roy Allen Nidoy <allen@gappify.com>
2025-01-16 06:55:48 +0800

    updated skills

--- src/data/skills_data.js
+++ src/data/skills_data.js
@@ -49,50 +49,50 @@ export const skills_medium = [
     label: "Cypress",
     logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cypressio/cypressio-original.svg",
   },
-  {
-    label: "Figma",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
-  },
+  // {
+  //   label: "Figma",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+  // },
   {
     label: "Amazon Web Services (AWS)",
     logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg",
   },
-  {
-    label: "Postman",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg",
-  },
+  // {
+  //   label: "Postman",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg",
+  // },
 ];
 export const skills_low = [
-  {
-    label: "Java",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
-  },
-  {
-    label: "Python",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
-  },
-  {
-    label: ".NET",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dot-net/dot-net-original.svg",
-  },
-  {
-    label: "SQLite",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg",
-  },
-  {
-    label: "Android Studio",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg",
-  },
-  {
-    label: "Electron",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/electron/electron-original.svg",
-  },
-  {
-    label: "Heroku",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg",
-  },
-  {
-    label: "Tailwind",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
-  },
+  // {
+  //   label: "Java",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+  // },
+  // {
+  //   label: "Python",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+  // },
+  // {
+  //   label: ".NET",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dot-net/dot-net-original.svg",
+  // },
+  // {
+  //   label: "SQLite",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg",
+  // },
+  // {
+  //   label: "Android Studio",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg",
+  // },
+  // {
+  //   label: "Electron",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/electron/electron-original.svg",
+  // },
+  // {
+  //   label: "Heroku",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg",
+  // },
+  // {
+  //   label: "Tailwind",
+  //   logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
+  // },
 ];
